6a7e2db1c13f3ad992952dc57c736cc3ea1d2ba4
SamiralVdB <63337189+SamiralVdB@users.noreply.github.com>
2024-05-24 15:02:21 +0200

    Add files via upload

--- BioCircuitSim_functions.py
+++ BioCircuitSim_functions.py
@@ -1,7 +1,12 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
-
+from typing import Tuple
+from pydna.amplify import pcr
+from pydna.dseqrecord import Dseqrecord
+import random
+import time
+import warnings
 
 def random_sequence_generator(length:int) -> str:
    nucleotides = ['A', 'T', 'C', 'G']
@@ -27,9 +32,28 @@ def get_putida_genome(promoter:str, regulator:str) -> str:
     putida_genome += random_sequence_generator(random.randint(0, 1000))
     return putida_genome
 
+def perform_pcr(forward_primer, reverse_primer, genome:str) -> Tuple[Dseqrecord, bool]:
+    #set new seed based on the time
+    t = 1e3 * time.time()
+    random.seed(int(t) % 2**32)
 
-def perform_batch_experiment(mutant, insert):
+    #randomly make the pcr work or not
+    if 0.5<random.uniform(0,1):
+        pcr_product = pcr(forward_primer, reverse_primer, genome)
+        return pcr_product, True
+    else:
+        #hybridized primers
+        primer = Dseqrecord((forward_primer.seq+reverse_primer.seq+forward_primer.seq)*2)
+        primer.add_feature()
+        return primer, False
+
+def perform_batch_experiment(mutant, pcr_product:Tuple):
     time = np.arange(0, 10)
+    #PCR failes
+    if not pcr_product[1]:
+        warnings.warn('PCR has failed, mutant does not contain insert. Please try to run the PCR again')
+        return
+    insert = pcr_product[0]
     if len(insert.seq) == 56 + 115:
         glc = [5, 4, 3, 2, 1, 0, 0, 0, 0, 0]
         glyc = [5, 4, 3, 2, 1, 0, 0, 0, 0, 0]
@@ -65,7 +89,7 @@ def perform_batch_experiment(mutant, insert):
 
     plt.plot(time, glc, label= 'glucose [$g_{glc}/L$]')
     plt.plot(time, glyc,  label= 'glycerol [$g_{glyc}/L$]')
-    plt.xlabel('concentration [g/L]')
-    plt.ylabel('time[h]')
+    plt.xlabel('time [h]')
+    plt.ylabel('concentration [g/L]')
     plt.legend()
     plt.show()
